2a27c369466b8b7c59dc9f73c681a1549a9e1751
Fujihara <fujihara.kosuke700@mail.kyutech.jp>
2020-11-26 10:30:54 +0900

    動画保存完成

--- camera/camera3.py
+++ camera/camera3.py
@@ -1,18 +1,25 @@
 import cv2
 import numpy as np
 import datetime as dt
+import sys
+sys.path.append("C:\\Users\\admin.H120\\Documents\\git\\linecar_fuji\\cali\\")
+import calibrate as cali
+
  
 cap = cv2.VideoCapture(0+cv2.CAP_DSHOW)
+#cali.calibrateImage()
  
 #保存
 fmt = cv2.VideoWriter_fourcc('m', 'p', '4', 'v')
 fps = 60.0
 size = (1280, 720)
 
+SAVE_PATH = "C:\\Users\\admin.H120\\Documents\\git\\linecar_fuji\\data\\camera\\"
+
 
 now = dt.datetime.now()
-time = now.strftime('%Y%m%d-%H%M%S')
-writer = cv2.VideoWriter('data/camera/video_{}.mp4'.format(time), fmt, fps, size)
+time = now.strftime('%Y%m%d_%H%M_%S')
+writer = cv2.VideoWriter(SAVE_PATH + 'video_{}.mp4'.format(time), fmt, fps, size)
  
 while True:
     _, frame = cap.read()
