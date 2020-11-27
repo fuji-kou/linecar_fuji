85a8d21b27267c89d9baefaf6eb7792871a4642c
Fujihara <fujihara.kosuke700@mail.kyutech.jp>
2020-11-27 16:16:20 +0900

    name change

--- camera/camera.py
+++ camera/camera.py
@@ -1,44 +1,40 @@
 import cv2
 import numpy as np
-import datetime
-
-camera = cv2.VideoCapture(0+cv2.CAP_DSHOW)
-print(type(camera))
-# <class 'cv2.VideoCapture'>
-print(camera.isOpened())
-# True
-
-#FPS,解像度の設定
-camera.set(cv2.CAP_PROP_FPS, 60)
-camera.set(cv2.CAP_PROP_FRAME_WIDTH, 1280)
-camera.set(cv2.CAP_PROP_FRAME_HEIGHT, 720)
-#cap.set(cv2.CAP_PROP_FOURCC, cv2.VideoWriter_fourcc('Y','U','Y','V'))
-
-
-#動画ファイル保存用の設定
-fps = int(camera.get(cv2.CAP_PROP_FPS))                    # カメラのFPSを取得
-w = int(camera.get(cv2.CAP_PROP_FRAME_WIDTH))              # カメラの横幅を取得
-h = int(camera.get(cv2.CAP_PROP_FRAME_HEIGHT))             # カメラの縦幅を取得
-fourcc = cv2.VideoWriter_fourcc('m', 'p', '4', 'v')        # 動画保存時のfourcc設定（mp4用)
-#now = datetime.datetime.now()
-#filename = 'camera_log' + now.strftime('%Y%m%d_%H%M%S') + '.mp4'
-#f = open(filename, 'w')
-#writer = writer(f, lineterminator='\n') 
-video = cv2.VideoWriter('video.mp4', fourcc, fps, (w, h))  # 動画の仕様（ファイル名、fourcc, FPS, サイズ）
+import datetime as dt
+import sys
+sys.path.append("C:\\Users\\admin.H120\\Documents\\git\\linecar_fuji\\cali\\")
+import calibrate as cali
 
  
-# 撮影＝ループ中にフレームを1枚ずつ取得（qキーで撮影終了）
+#cali.calibrateImage() 
+cap = cv2.VideoCapture(0+cv2.CAP_DSHOW)
+ 
+#保存
+fmt = cv2.VideoWriter_fourcc('m', 'p', '4', 'v')
+fps = 60.0
+size = (1280, 720)
+
+SAVE_PATH = "C:\\Users\\admin.H120\\Documents\\git\\linecar_fuji\\data\\camera\\"
+
+
+now = dt.datetime.now()
+time = now.strftime('%Y%m%d_%H%M_%S')
+writer = cv2.VideoWriter(SAVE_PATH + 'video_{}.mp4'.format(time), fmt, fps, size)
+ 
 while True:
-    ret, frame = camera.read()              # フレームを取得
-    cv2.imshow('camera', frame)             # フレームを画面に表示
-    video.write(frame)                      # 動画を1フレームずつ保存する
- 
-    # キー操作があればwhileループを抜ける
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        video.write(frame)                      # 動画を1フレームずつ保存する
+    ret, frame = cap.read()
+    print(ret)                          #ret:フレームを取得できたかTrueかFalseで示す
+    frame = cv2.resize(frame, size)
+     
+    #保存
+    writer.write(frame)
+     
+    cv2.imshow('frame', frame)
+    #qキーで終了
+    if cv2.waitKey(1) == ord('q'):
         break
  
-# 撮影用オブジェクトとウィンドウの解放
-camera.release()
-video.release()
+#保存
+writer.release()
+cap.release()
 cv2.destroyAllWindows()
