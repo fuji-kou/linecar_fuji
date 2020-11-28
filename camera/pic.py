f88f1ee74127e9afa15c809afc851154f2fc53a3
Fujihara <fujihara.kosuke700@mail.kyutech.jp>
2020-11-28 17:20:27 +0900

    整理

--- camera/pic.py
+++ camera/pic.py
@@ -1,5 +1,15 @@
 import cv2
-import os
+import numpy as np
+import datetime as dt
+import sys
+from time import sleep
+
+TMP_FOLDER_PATH = "C:\\Users\\admin.H120\\Documents\\git\\linecar_fuji\\cali\\tmp"
+MTX_PATH = TMP_FOLDER_PATH + "\\mtx2.csv"
+DIST_PATH = TMP_FOLDER_PATH + "\\dist2.csv"
+
+
+
 
 def save_frame_camera_key(device_num, dir_path, basename, ext='jpg', delay=1, window_name='frame'):
     cap = cv2.VideoCapture(0+cv2.CAP_DSHOW)
@@ -21,7 +31,9 @@ def save_frame_camera_key(device_num, dir_path, basename, ext='jpg', delay=1, wi
     n = 0
     while True:
         ret, frame = cap.read()
-        cv2.imshow(window_name, frame)
+        #cv2.imshow(window_name, frame)
+
+
         key = cv2.waitKey(delay) & 0xFF
         if key == ord('c'):
             cv2.imwrite('{}_{}.{}'.format(base_path, n, ext), frame)
