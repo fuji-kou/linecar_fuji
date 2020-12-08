989f1f272bdc6b52ab9462677bad57a5f663f99c
Fujihara <fujihara.kosuke700@mail.kyutech.jp>
2020-12-08 18:09:40 +0900

    赤を2個検出に変更

--- camera/camera_settings.py
+++ camera/camera_settings.py
@@ -48,59 +48,45 @@ class camera():
         # ブロブ情報を項目別に抽出
         n = label[0] - 1
         data = np.delete(label[2], 0, 0)
-        #print(data)
         center = np.delete(label[3], 0, 0)
 
-        # ブロブ面積最大のインデックス
-        max_index = np.argmax(data[:, 4])
-        #print(max_index)
-        #print(data[max_index][4])
-        #data2 = sorted(data, key=lambda x :x[4])
-        #pprint.pprint(data2[-1][4])
-
+        data2 = sorted(data, key=lambda x :x[4])
 
         # 面積最大ブロブの情報格納用
         maxblob = {}
 
         # 面積最大ブロブの各種情報を取得
-        maxblob["upper_left"] = (data[:, 0][max_index], data[:, 1][max_index]) # 左上座標
-        maxblob["width"] = data[:, 2][max_index]  # 幅
-        maxblob["height"] = data[:, 3][max_index]  # 高さ
-        maxblob["area"] = data[:, 4][max_index]   # 面積
-        maxblob["center"] = center[max_index]  # 中心座標
-        # print(maxblob)
-        # if len(data2) == 0:
-        #     # pass
-        
-        # if len(data2) == 1:
-        #     #camera1
-        #     maxblob["upper_left1"] = (data2[-1][0], data2[-1][1]) # 左上座標
-        #     maxblob["width1"] = data2[-1][2]  # 幅
-        #     maxblob["height1"] = data2[-1][3]  # 高さ
-        #     maxblob["area1"] = data2[-1][4]   # 面積
-        #     maxblob["center1"] = (data2[-1][0]+data2[-1][2]/2,data2[-1][1]+data2[-1][3]/2)  # 中心座標
+        if len(data2) == 0:
+            pass
+        if len(data2) == 1:
+            #camera1
+            maxblob["upper_left1"] = (data2[-1][0], data2[-1][1]) # 左上座標
+            maxblob["width1"] = data2[-1][2]  # 幅
+            maxblob["height1"] = data2[-1][3]  # 高さ
+            maxblob["area1"] = data2[-1][4]   # 面積
+            maxblob["center1"] = (data2[-1][0]+data2[-1][2]/2,data2[-1][1]+data2[-1][3]/2)  # 中心座標
 
-        #     #camera2
-        #     maxblob["upper_left2"] = (0,0)# 左上座標
-        #     maxblob["width2"] = 0  # 幅
-        #     maxblob["height2"] = 0  # 高さ
-        #     maxblob["area2"] = 0   # 面積
-        #     maxblob["center2"] = (0,0)
+            #camera2
+            maxblob["upper_left2"] = (0,0)# 左上座標
+            maxblob["width2"] = 0  # 幅
+            maxblob["height2"] = 0  # 高さ
+            maxblob["area2"] = 0   # 面積
+            maxblob["center2"] = (0,0)
      
-        # else:
-        #     #camera1
-        #     maxblob["upper_left1"] = (data2[-1][0], data2[-1][1]) # 左上座標
-        #     maxblob["width1"] = data2[-1][2]  # 幅
-        #     maxblob["height1"] = data2[-1][3]  # 高さ
-        #     maxblob["area1"] = data2[-1][4]   # 面積
-        #     maxblob["center1"] = (data2[-1][0]+data2[-1][2]/2,data2[-1][1]+data2[-1][3]/2)  # 中心座標
+        else:
+            #camera1
+            maxblob["upper_left1"] = (data2[-1][0], data2[-1][1]) # 左上座標
+            maxblob["width1"] = data2[-1][2]  # 幅
+            maxblob["height1"] = data2[-1][3]  # 高さ
+            maxblob["area1"] = data2[-1][4]   # 面積
+            maxblob["center1"] = (data2[-1][0]+data2[-1][2]/2,data2[-1][1]+data2[-1][3]/2)  # 中心座標
 
-        #     #camera2
-        #     maxblob["upper_left2"] = (data2[-2][0], data2[-2][1]) # 左上座標
-        #     maxblob["width2"] = data2[-2][2]  # 幅
-        #     maxblob["height2"] = data2[-2][3]  # 高さ
-        #     maxblob["area2"] = data2[-2][4]   # 面積
-        #     maxblob["center2"] = (data2[-2][0]+data2[-2][2]/2,data2[-2][1]+data2[-2][3]/2)  # 中心座標
+            #camera2
+            maxblob["upper_left2"] = (data2[-2][0], data2[-2][1]) # 左上座標
+            maxblob["width2"] = data2[-2][2]  # 幅
+            maxblob["height2"] = data2[-2][3]  # 高さ
+            maxblob["area2"] = data2[-2][4]   # 面積
+            maxblob["center2"] = (data2[-2][0]+data2[-2][2]/2,data2[-2][1]+data2[-2][3]/2)  # 中心座標
 
         #     # print(maxblob)
             
