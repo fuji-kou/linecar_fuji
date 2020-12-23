1a63e0fb3a9cd39d859bdb778ca146673fbb07b9
fuji-kou <fujihara.kosuke700@mail.kyutech.jp>
2020-12-23 18:42:44 +0900

    socket

--- socket/client.py
+++ socket/client.py
@@ -1,6 +1,10 @@
 import socket
-import time
+from time import sleep
 import RPi.GPIO as GPIO
+import sys
+import os
+sys.path.append('..')
+import linecar_settings as sets
 
 
 # pin設定
@@ -24,11 +28,12 @@ def mv_angle(angle):
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     # サーバを指定
-    s.connect(('127.0.0.1', 50007))
-    #s.connect(('192.168.43.198', 50007))
+    #s.connect(('127.0.0.1', 50007))
+    s.connect(('192.168.43.198', 50007))
     # サーバにメッセージを送る
-    #s.sendall(b'hello')
+    s.sendall(b'hello')
     # ネットワークのバッファサイズは1024。サーバからの文字列を取得する
     data = s.recv(1024)
     #
     print(repr(data))
+    p1.start(10)
