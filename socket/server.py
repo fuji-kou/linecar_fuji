4c98eff4b09db64ea1d90eca973bb7425f8ba4c6
Fujihara <fujihara.kosuke700@mail.kyutech.jp>
2020-12-23 13:01:20 +0900

    修正

---
+++ socket/server.py
@@ -0,0 +1,31 @@
+import socket
+import time
+
+start = time.time()
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:    #ソケット作成
+    # IPアドレスとポートを指定
+    #s.bind(('127.0.0.1', 50007))
+    #s.bind(('255.255.255.0', 50007))
+    s.bind(('192.168.43.198', 50007))
+    #  接続(最大2)
+    s.listen(2)
+    # connection するまで待つ
+    while True:
+        # 誰かがアクセスしてきたら、コネクションとアドレスを入れる
+        conn, addr = s.accept()
+        with conn:
+            while True:
+                # データを受け取る
+                data = conn.recv(1024)
+                if not data:
+                    break
+                print('data : {}, addr: {}'.format(data, addr))
+                # クライアントにデータを返す(b -> byte でないといけない)
+                conn.sendall(b'Received: ' + data)
+                # if time.time() - start > 5:
+                #     break
+
+
+    # except KeyboardInterrupt:
+    #     print('!!FINISH!!')
