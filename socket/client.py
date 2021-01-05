2cd57b701715476ccc682b4cc301625f76facba5
Fujihara <fujihara.kosuke700@mail.kyutech.jp>
2021-01-05 15:21:01 +0900

    socket

--- socket/client.py
+++ socket/client.py
@@ -1,11 +1,33 @@
 import socket
+import time
+import RPi.GPIO as GPIO
+
+
+# pin設定
+GPIO.setmode(GPIO.BCM)      
+GPIO.setwarnings(False)             #GPIOからの警告を有効にする
+
+GPIO.setup(sets.pwm, GPIO.OUT)      #出力設定          
+GPIO.setup(sets.DIR, GPIO.OUT)
+GPIO.setup(sets.Servo_pin, GPIO.OUT)  
+sleep(1)
+
+p1 = GPIO.PWM(sets.pwm, 100)            #pwmピンの設定
+Servo = GPIO.PWM(sets.Servo_pin, 50) 
+Servo.start(0)                      
+
+# servo
+def mv_angle(angle):
+    duty = 2.5 + (12.0 - 2.5) * (angle + 90) / 180   #角度からデューティ比を求める
+    Servo.ChangeDutyCycle(duty)     #デューティ比を変更
+
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     # サーバを指定
     s.connect(('127.0.0.1', 50007))
     #s.connect(('192.168.43.198', 50007))
     # サーバにメッセージを送る
-    s.sendall(b'hello')
+    #s.sendall(b'hello')
     # ネットワークのバッファサイズは1024。サーバからの文字列を取得する
     data = s.recv(1024)
     #
