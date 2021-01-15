451ff7e14b0565bda60ef5e27b711ff7fc7a172d
fuji-kou <fujihara.kosuke700@mail.kyutech.jp>
2021-01-15 18:52:16 +0900

    条件追加

--- socket/client_left.py
+++ socket/client_left.py
@@ -29,11 +29,13 @@ sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 # サーバを指定
 #同端末
 #sock.connect(('127.0.0.1', 50007))
-#ファーウェイタブ（ラズパイとの通信）
-#sock.connect(('192.168.43.198', 50007))
+#ファーウェイタブ（ラズパイとの通信）my_PC
+sock.connect(('192.168.43.198', 50007))
 #実機パソコン
 #sock.connect(('192.168.179.2', 50007))
-sock.connect(('192.168.11.34',50008))
+#sock.connect(('192.168.11.34',50008))
+
+#sock.connect(('192.168.11.12',50009))
 
 # サーバにメッセージを送る
 while True:
@@ -47,6 +49,7 @@ while True:
         print(data)
         mv_angle(0)
         p1.start(sets.SPEED)
+        sleep(5)
     #離れすぎたら前に出る
     if data == (b'Go1'):
         print(data)
@@ -66,18 +69,21 @@ while True:
         pass
     #範囲から出たとき
     if data == (b'turn_left1'):
-        mv_angle(5)
+        mv_angle(20)
         p1.start(sets.turn_SPEED)
+        sleep(5) 
     if data == (b'turn_left2'):
         pass
     #範囲から出たとき
     if data == (b'turn_right1'):
-        mv_angle(-5)
+        mv_angle(-20)
         p1.start(sets.turn_SPEED)
+        sleep(5)
     if data == (b'turn_right2'):
         pass
 
     if data == 0:
+        mv_angle(0)
         break
 
 
